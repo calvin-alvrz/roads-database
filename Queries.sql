4146d8e3d8e12b2e9a966712295f019421a55d8b
Calvin Alvarez <calvin_alvarez@outlook.com>
2023-10-18 15:47:36 +1300

    queries update

--- Queries.sql
+++ Queries.sql
@@ -4,8 +4,13 @@
 are going to end within the first week of October of 2023 and retrieve the following details
 (rd_id, rd_name, proj_code, proj_name, proj_end_date, proj_status)*/
 
-SELECT r.rd_id AS "Road ID", r.rd_name AS "Road Name", p.proj_code AS "Project Code",
-p.proj_name AS "Project Name", p.proj_end_date AS "Project End Date", pr.proj_status AS "Status"
+SELECT 
+    r.rd_id AS "Road ID",
+    r.rd_name AS "Road Name",
+    p.proj_code AS "Project Code",
+    p.proj_name AS "Project Name",
+    p.proj_end_date AS "Project End Date",
+    pr.proj_status AS "Status"
 FROM roads r, projects p, projroad pr
 WHERE pr.projectsproj_code = p.proj_code
 AND r.rd_id = pr.roadsrd_id
@@ -18,9 +23,11 @@ AND proj_end_date < '08-10-23';
 and retrieves the following details (emp_first_name, emp_last_name, emp_phone, emp_email,
 emp_status)*/
 
-SELECT e.emp_first_name || ' ' || e.emp_last_name AS "Full Name",
-e.emp_phone AS "Phone Number", LOWER(e.emp_email) || '@atmobile.com' AS "Email",
-ep.emp_status AS "Status"
+SELECT
+    e.emp_first_name || ' ' || e.emp_last_name AS "Full Name",
+    e.emp_phone AS "Phone Number",
+    LOWER(e.emp_email) || '@atmobile.com' AS "Email",
+    ep.emp_status AS "Status"
 FROM employees e, empproj ep
 WHERE e.emp_id = ep.employeesemp_id
 AND ep.emp_status = 'Sick Leave';
@@ -32,9 +39,15 @@ holds a managerial position within a contract. It retrieves the following detail
 (emp_first_name, emp_last_name, job_name, cnt_number, cnt_name, cnt_start_date,
 cnt_end_date, and days in the position) */
 
-SELECT e.emp_first_name || ' ' || e.emp_last_name AS "Full Name", j.job_name AS "Job Title",
-c.cnt_number AS "Contract Number", c.cnt_name AS "Contract Name",c.cnt_start_date AS "Contract Start Date",
-c.cnt_end_date AS "Contract End Date", ROUND(MONTHS_BETWEEN(c.cnt_end_date, c.cnt_start_date), 0) AS "# Months in Position"
+SELECT
+    e.emp_first_name || ' ' || e.emp_last_name AS "Full Name",
+    j.job_name AS "Job Title",
+    c.cnt_number AS "Contract Number",
+    c.cnt_name AS "Contract Name",
+    c.cnt_start_date AS "Contract Start Date",
+    c.cnt_end_date AS "Contract End Date",
+    ROUND(MONTHS_BETWEEN(c.cnt_end_date,
+    c.cnt_start_date), 0) AS "# Months in Position"
 FROM employees e, contracts c, jobs j, empjobs ej, cntjobs cj
 WHERE e.emp_id = ej.employeesemp_id
 AND j.job_name = ej.jobsjob_name
@@ -45,14 +58,9 @@ AND UPPER(j.job_name) LIKE '%MANAGER';
 --! Query 4:
 
 /*Purpose of the query is to create a report with the subsections of roads that
-are longer than 2 kilometres and order them in ascending order by the road's name.
-It retrieves the following details (rd_id, rd_id, road_name,rd_est_length) */
+are longer than 2 kilometres and order them in ascending order by the road's name. */
 
-SELECT r.rd_id, o.rd_id, r.road_name, rl.rd_est_length
-FROM roads r, roads o, roadloc rl
-WHERE r.rd_id = o.rd_id
-AND r.rd_id = rl.roadsrd_id
-ORDER BY ASC r.road_name;
+
 
 --! Query 5:
 
